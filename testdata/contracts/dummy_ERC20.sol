7e4c8caa3754ca4d908858b08dde56a61aa6a798
Shivam Sharma <shivam691999@gmail.com>
2025-04-21 21:57:26 +0530

    add : e2eTest

---
+++ testdata/contracts/dummy_ERC20.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ERC20 {
+    string public name;
+    string public symbol;
+
+    constructor(string memory _name, string memory _symbol) {
+        name = _name;
+        symbol = _symbol;
+    }
+}
